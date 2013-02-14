e5627de64ce252255f3f642f85a4f7ecbcc0b6a0
Guiwei Lin <guiwei@apptastic.com.au>
2013-02-14 21:26:50 +1100

    change production env setting

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,11 @@ module MyResume
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+
+    config.autoload_paths += %W(#{config.root}/lib)
+
+
+
   end
 end
